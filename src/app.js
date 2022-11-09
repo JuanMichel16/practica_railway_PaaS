654ba4cc8ea253e84089c34548f62dfa51d0e9bf
JuanMichel16 <juan.michel16@outlook.com>
2022-11-09 13:47:05 -0600

    primer commit

---
+++ src/app.js
@@ -0,0 +1,60 @@
+import express from "express";
+const app=express();
+
+import { createPool } from "mysql2/promise";
+
+ 
+
+const pool=createPool({
+
+    user:'root',
+
+    password:'zej6j4lqjhjWWEhO2Xg2',
+
+    host:'containers-us-west-114.railway.app',
+
+    port:6262,
+
+    database:'railway'
+
+})
+
+app.get('/',(req,res)=>{
+
+  res.send("Bienvenido a este servidor..")      
+
+})
+
+ 
+
+app.get('/usuarios',async (req,res)=>{
+
+    const [result] = await pool.query('select * from usuario')
+
+    res.json(result[0])      
+
+  })
+
+ 
+
+  app.get('/agregarusuario',async (req,res)=>{
+
+    const nombre=req.query.nombre
+
+    const contrasena=req.query.contrasena
+
+    const correo=req.query.correo
+
+    const tienda=req.query.tienda  
+
+    const [result]=await pool.query(`INSERT INTO usuario (nombre, contrasena, correo, tienda) VALUES ('${nombre}', '${contrasena}', '${correo}','${tienda}')`)
+
+    res.json(result[0])      
+
+  })
+
+ 
+
+app.listen(process.env.PORT || 3000)
+
+console.log("Servidor corriendo en el puerto 3000")
